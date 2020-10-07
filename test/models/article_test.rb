eaef95e14520cae5ec5a6a51dafb36157836900a
kota_koyama <kouta103177@gmail.com>
2020-10-07 22:43:54 +0900

    Article_model作成

---
+++ test/models/article_test.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
