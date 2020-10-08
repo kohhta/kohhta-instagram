e134516e6dbd03992b668383408085335fb207cc
kota_koyama <kouta103177@gmail.com>
2020-10-08 23:22:05 +0900

    createアクションを追加,投稿保存

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,22 @@ class ArticlesController < ApplicationController
   end
   
   def new
-    
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
   end
   
+
+  private
+  def article_params
+    params.require(:article).permit(:content)
+  end
 
 end
