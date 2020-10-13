ae0c0f7494d97d291521b2a5d6905ac7b2adc463
kota_koyama <kouta103177@gmail.com>
2020-10-13 19:12:52 +0900

    show comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   root to: 'articles#index'
 
   resource :profile, only: [:show, :edit, :update]
-  resources :articles
+
+  resources :articles do
+      resources :comments, only: [:new, :create]
+  end
 
 end
