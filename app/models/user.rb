8cad66c4557695c57dac9695e0b6c2b2784442f0
kota_koyama <kouta103177@gmail.com>
2020-10-21 14:47:18 +0900

    relationships テーブルを作成,フォローの関係 ,フォロワーの関係 ,フォローを外す

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,6 +30,12 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :favorite_articles, through: :likes, source: :article
 
+  has_many :following_relationships, foreign_key: 'follower_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: 'following_id', class_name: 'Relationship', dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   
@@ -45,6 +51,17 @@ class User < ApplicationRecord
   def display_name
     profile&.nickname || self.username
   end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
+  end
+  
+  
 
   def prepare_profile
     profile || build_profile
