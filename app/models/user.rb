55d497dee7275a93df5b457d20b661388a5e0e54
kota_koyama <kouta103177@gmail.com>
2020-10-21 16:29:52 +0900

    アンフォロー機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable
 
   # has_one :profile, dependent: :destroy
   has_many :articles, dependent: :destroy
@@ -53,16 +53,13 @@ class User < ApplicationRecord
   end
 
   def follow!(user)
-    if user. is_a?(User)
-      user_id = user.id 
-    else
-      user_id = user
-    end
+    user_id = get_user_id(user)
     following_relationships.create!(following_id: user_id)
   end
 
   def unfollow!(user)
-    relation = following_relationships.find_by!(following_id: user.id)
+    user_id =  get_user_id(user)
+    relation = following_relationships.find_by!(following_id: user_id)
     relation.destroy!
   end
 
@@ -70,9 +67,6 @@ class User < ApplicationRecord
     following_relationships.exists?(following_id: user.id)
     
   end
-  
-  
-  
 
   def prepare_profile
     profile || build_profile
@@ -85,5 +79,15 @@ class User < ApplicationRecord
       'default-avatar.png'
     end
   end
+
+  private
+  def get_user_id(user)
+    if user. is_a?(User)
+      user.id 
+    else
+      user
+    end
+  end
+  
   
 end
