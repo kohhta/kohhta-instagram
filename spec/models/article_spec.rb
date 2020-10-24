88ee9ef2f7ee6d94d631008e47f17a363e317870
kota_koyama <kouta103177@gmail.com>
2020-10-24 18:02:16 +0900

    article factoryを登録

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,23 +3,20 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   let!(:user) { create(:user) }
 
-  context 'タイトルと内容が入力されている場合' do
-    let!(:article) do
-      user.articles.build({
-        content: Faker::Lorem.characters(number: 30)
-      })
-    end
-    it '記事を保存できる' do
+  context '内容が入力されている場合' do
+    let!(:article) { build(:article, user: user) }
 
+    it '記事を保存できる' do
       expect(article).to be_valid
     end
   end
 
   context 'コンテントの文字ない場合' do
-    let!(:article) do
-      user.articles.create({
-        content: Faker::Lorem.characters(number: 0)
-      })
+    let!(:article) { build(:article, content:  Faker::Lorem.characters(number: 0), user: user) }
+    # ここでダミーデータ作成。0文字の場合
+
+    before do
+      article.save
     end
 
     it '記事を保存できない' do
