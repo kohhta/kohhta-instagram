0c02fdcb9e3063145b208216e4b74d39f84302a2
kota_koyama <kouta103177@gmail.com>
2020-10-24 14:24:12 +0900

    エラー処理,axios export default

---
+++ app/javascript/modules/axios.js
@@ -0,0 +1,10 @@
+// axios 再度定義するのが面倒な場合使用
+
+// import axios from 'axios'
+// import {
+//   csrfToken
+// } from 'rails-ujs'
+
+// axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
+// export default axios
