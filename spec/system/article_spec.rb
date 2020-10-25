500e3e7e0c9cb5d1b79933817ab40404cf26f015
kota_koyama <kouta103177@gmail.com>
2020-10-25 11:39:21 +0900

    記事一覧のコンテントが表示されているか確認

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Article', type: :system do
+
+  let!(:user) { create(:user) }
+  let!(:articles) { create_list(:article, 3, user: user) }
+
   it '記事一覧がひょうじされる' do
     visit root_path
+    articles.each do |article|
+      expect(page).to have_content(article.content)
+    end
   end
 end
