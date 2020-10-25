31137b1e0d7c433d4d0a53660f5ad4610f48ed73
kota_koyama <kouta103177@gmail.com>
2020-10-25 12:09:37 +0900

    system spec

--- spec/system/article_spec.rb
+++ spec/system/article_spec.rb
@@ -1,11 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Article', type: :system do
-
   let!(:user) { create(:user) }
   let!(:articles) { create_list(:article, 3, user: user) }
 
-  it '記事一覧がひょうじされる' do
+  it '記事一覧が表示される' do
     visit root_path
     articles.each do |article|
       expect(page).to have_content(article.content)
