4637f38f79ab846aab5633e2ab01dbab68d3e901
kota_koyama <kouta103177@gmail.com>
2020-11-10 16:22:27 +0900

    プロフィール画像をクリックしたら画像を選択

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,16 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-
 require("@rails/activestorage").start()
 require("channels")
+
+
+
+import $ from 'jquery'
+import axios from 'axios'
+import { csrfToken } from 'rails-ujs'
+axios.defaults.headers.common['X-CSRF-Token'] = csrfToken()
+
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -15,3 +22,38 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+document.addEventListener('DOMContentLoaded', () => {
+  const reader = new FileReader();
+  const imageUpload = document.getElementById("post_img");
+
+imageUpload.onchange = function() {
+  var file = $('input[type="file"]').prop('files')[0];
+
+  if(!file) {
+    window.alert('画像を選択してください')
+  } else {
+    reader.readAsDataURL(file);
+    reader.onload = function (e) {
+
+      $('#avatar_img_prev').attr('src', e.target.result);
+      axios.put(`/profile/edit`)
+      
+        .then((res) => {
+          window.alert('成功！')
+
+          $('#avatar_img_prev').removeClass('hidden');
+          $('.avatar_present_img').remove();
+        })
+
+        .catch((e) => {
+          
+        })
+    }
+  }
+}
+
+$('.avatar_present_img').on('click', () => {
+  $('#post_img').click()
+})
+});
